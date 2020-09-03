d1973e89a9046b73e6f539387681e2371540af15
JohanSnurre <9904jola@gmail.com>
2020-09-03 17:00:41 +0200

    first

---
+++ inlämningsuppgift1_forms/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace inlämningsuppgift1_forms
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtBox_price = new System.Windows.Forms.TextBox();
+            this.txtBox_paid = new System.Windows.Forms.TextBox();
+            this.lbl_answer = new System.Windows.Forms.Label();
+            this.lbl_price_name = new System.Windows.Forms.Label();
+            this.lbl_payment_name = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtBox_price
+            // 
+            this.txtBox_price.Location = new System.Drawing.Point(109, 51);
+            this.txtBox_price.Name = "txtBox_price";
+            this.txtBox_price.Size = new System.Drawing.Size(100, 20);
+            this.txtBox_price.TabIndex = 0;
+            this.txtBox_price.Text = "0";
+            this.txtBox_price.TextChanged += new System.EventHandler(this.txtBox_price_TextChanged);
+            // 
+            // txtBox_paid
+            // 
+            this.txtBox_paid.Location = new System.Drawing.Point(109, 126);
+            this.txtBox_paid.Name = "txtBox_paid";
+            this.txtBox_paid.Size = new System.Drawing.Size(100, 20);
+            this.txtBox_paid.TabIndex = 1;
+            this.txtBox_paid.Text = "0";
+            this.txtBox_paid.TextChanged += new System.EventHandler(this.txtBox_paid_TextChanged);
+            // 
+            // lbl_answer
+            // 
+            this.lbl_answer.AutoSize = true;
+            this.lbl_answer.Location = new System.Drawing.Point(337, 51);
+            this.lbl_answer.Name = "lbl_answer";
+            this.lbl_answer.Size = new System.Drawing.Size(17, 13);
+            this.lbl_answer.TabIndex = 2;
+            this.lbl_answer.Text = "lbl";
+            this.lbl_answer.Click += new System.EventHandler(this.lbl_answer_Click);
+            // 
+            // lbl_price_name
+            // 
+            this.lbl_price_name.AutoSize = true;
+            this.lbl_price_name.Location = new System.Drawing.Point(79, 54);
+            this.lbl_price_name.Name = "lbl_price_name";
+            this.lbl_price_name.Size = new System.Drawing.Size(24, 13);
+            this.lbl_price_name.TabIndex = 3;
+            this.lbl_price_name.Text = "Pris";
+            // 
+            // lbl_payment_name
+            // 
+            this.lbl_payment_name.AutoSize = true;
+            this.lbl_payment_name.Location = new System.Drawing.Point(15, 129);
+            this.lbl_payment_name.Name = "lbl_payment_name";
+            this.lbl_payment_name.Size = new System.Drawing.Size(88, 13);
+            this.lbl_payment_name.TabIndex = 4;
+            this.lbl_payment_name.Text = "Betalningsmängd";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(597, 411);
+            this.Controls.Add(this.lbl_payment_name);
+            this.Controls.Add(this.lbl_price_name);
+            this.Controls.Add(this.lbl_answer);
+            this.Controls.Add(this.txtBox_paid);
+            this.Controls.Add(this.txtBox_price);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtBox_price;
+        private System.Windows.Forms.TextBox txtBox_paid;
+        private System.Windows.Forms.Label lbl_answer;
+        private System.Windows.Forms.Label lbl_price_name;
+        private System.Windows.Forms.Label lbl_payment_name;
+    }
+}
+
---
+++ inlämningsuppgift1_forms/Form1.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace inlämningsuppgift1_forms
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            lbl_answer.Text = "Lägg in både pris och betalning \n i textlådorna till vänster";
+        }
+
+
+
+        private static int[] currencies = new int[7] { 500, 100, 50, 20, 10, 5, 1 };    //An array containing the currencies in a decreasing order
+        private static string[] nameOfValuesPlur = new string[7] { "femhundralappar", "hundralappar", "femtiolappar", "tjugolappar", "tiokronor", "femkronor", "enkronor" };     //Names for the currencies in plural
+        private static string[] nameOfValuesSing = new string[7] { "femhundralapp", "hundralapp", "femtiolapp", "tjugolapp", "tiokrona", "femkrona", "enkrona" };          //Names for the currencies in singular
+        
+        private void txtBox_price_TextChanged(object sender, EventArgs e)
+        {
+            
+            int price;                                              //max size for price is 2^31 -1
+            try
+            {                                                   //Testing that the user actually gives some amount of money
+                price = Int32.Parse(txtBox_price.Text);                      //Converting the input string to an int
+            }
+            catch (Exception err)
+            {
+                if (txtBox_price.Text.Equals(""))
+                {
+                    lbl_answer.Text = "Vänligen skriv in en summa pengar i respektive ruta";
+                    return;
+                }
+                lbl_answer.Text = "Någon av rutorna har en ogiltig summa med pengar";
+                return; 
+            }
+
+            if (price == 0)                                         //If the item is free, they  shouldn't have to pay anything
+            {
+                lbl_answer.Text = "Ta det, det är gratis";
+                return;
+            }
+            else if (price < 0)                                     //If the item has a negative cost, the buyer receives money for taking it
+            {
+                lbl_answer.Text = "Ta det, det är gratis. Och ta också " + -price + " kr";
+                return;
+            }
+            procedure();                                          //Returns the price
+
+        }
+
+        private void txtBox_paid_TextChanged(object sender, EventArgs e)
+        {
+            
+
+            int paid;                                           //max size for paid is 2^31 -1
+            try
+            {                                                   //Checks that the input is valid
+                paid = Int32.Parse(txtBox_paid.Text);
+            }
+            catch (Exception err)
+            {
+                if (txtBox_paid.Text.Equals(""))
+                {
+                    lbl_answer.Text = "Vänligen skriv in en summa pengar i respektive ruta";
+                    return;
+                }
+                lbl_answer.Text = "Någon av rutorna har en ogiltig summa med pengar";
+               
+            }
+
+            procedure();                                      //Returns the payment
+
+        }
+
+        private void procedure()                             //I know that this is a bad name for the function, but i couldn't come up with a better one
+        {
+            lbl_answer.Text = "";
+            int price = Int32.Parse(txtBox_price.Text);
+            if (price == 0 || price < 0) return;                    //The program shouldn't continue if the item is free of charge
+            int paid = Int32.Parse(txtBox_paid.Text);
+            if (paid < price)                                        //If the given payment is too low, tell customer that
+            {
+                lbl_answer.Text = "Ledsen, men du har inte tillräckligt med pengar";
+                return;
+            }
+
+            int difference = paid - price;
+            int[] resultingAmount = new int[7];                     //store the amount of each value in a new array
+            lbl_answer.Text = "Växel tillbaka: \n";
+            for (int i = 0; i < resultingAmount.Length; i++)        //Loop through each of the currencies and remove as many as possible of those from the difference
+            {                                                       //Add the amount of removed currencies to the resultingAmount array
+                
+                while (difference - currencies[i] >= 0)
+                {
+                    resultingAmount[i]++;
+                    difference -= currencies[i];
+                }
+                if (resultingAmount[i] == 0) continue;              //If the amount of a certain value is 0, it is not printed out
+
+                //Print out the resulting amount of currencies with the correct grammar
+                if (resultingAmount[i] > 1) lbl_answer.Text += resultingAmount[i] + " " + nameOfValuesPlur[i] + "\n";
+                else lbl_answer.Text += resultingAmount[i] + " " + nameOfValuesSing[i] + "\n";
+
+            }
+
+
+
+
+        }
+
+        private void lbl_answer_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
