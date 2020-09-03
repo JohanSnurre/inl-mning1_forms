4fd8474578b32df852d374c96e055d41177544d7
JohanSnurre <9904jola@gmail.com>
2020-09-03 17:10:28 +0200

    some bugs

--- inlämningsuppgift1_forms/Form1.cs
+++ inlämningsuppgift1_forms/Form1.cs
@@ -8,6 +8,8 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+//Author: Johan Larsson -> Johlax-8
+
 namespace inlämningsuppgift1_forms
 {
     public partial class Form1 : Form
